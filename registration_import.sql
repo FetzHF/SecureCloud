1bdde21c3d7e547e292ec7a3edca68c5f3e30156
FetzHF <fetzermee@gmail.com>
2014-10-12 00:35:01 -0400

    Added registration sql import file, also fixed up login template some

---
+++ registration_import.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 12, 2014 at 12:30 AM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `owncloud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oc_pending_regist`
+--
+
+CREATE TABLE IF NOT EXISTS `oc_pending_regist` (
+  `email` text NOT NULL,
+  `token` text NOT NULL,
+  `requested` int(11) NOT NULL,
+  `pending_regist_pKey` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
